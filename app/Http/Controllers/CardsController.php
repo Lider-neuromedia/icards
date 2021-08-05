f16a123941513fc54085d433bd9e4656f60652fb
Jose Nieto <inge1neuro@gmail.com>
2021-08-05 14:03:47 -0500

    Agregando datos de prueba y plantilla de ecards.

---
+++ app/Http/Controllers/CardsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Storage;
+
+class CardsController extends Controller
+{
+    public function card(String $card)
+    {
+        if (!Storage::exists("cards/$card.json")) {
+            return abort(404);
+        }
+
+        $data = json_decode(Storage::get("cards/$card.json"));
+        $ecard = $data->fields;
+
+        return view('card', compact('card', 'ecard'));
+    }
+}
