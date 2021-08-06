cec5ac7441abca4ae5b7ab6a82564cd6cdf31b0f
Jose Nieto <inge1neuro@gmail.com>
2021-08-06 17:37:54 -0500

    Agregando comandos de artisan a rutas web.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/ec/{card}', 'CardsController@card');
+
+Route::get('/setup', function () {
+    $exitCode = Artisan::call('storage:link');
+    return response()->json('done', 200);
+});
