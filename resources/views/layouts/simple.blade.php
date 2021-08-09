05c1995c8173ad4e40fff5aca066c1e377d7892e
Jose Nieto <inge1neuro@gmail.com>
2021-08-09 08:50:20 -0500

    Corrigiendo nombre y favicon.

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ url('favicon.ico') }}" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Ecard') }}</title>
+    <title>iCard</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
