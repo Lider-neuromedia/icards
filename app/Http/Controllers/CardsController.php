faa38bbd4c25ebe9ccc68a961ef51f6e70217bf0
Jose Nieto <inge1neuro@gmail.com>
2021-09-09 17:28:47 -0500

    Validando suscripción para visualizar tarjeta en público.

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Card;
 use App\CardField;
+use Carbon\Carbon;
 
 class CardsController extends Controller
 {
     public function card(String $card)
     {
-        $card = Card::whereSlug($card)->firstOrFail();
+        $card = Card::query()
+            ->whereSlug($card)
+            ->whereHas('client', function ($q) {
+                $q->whereHas('subscriptions', function ($q) {
+                    $q->where('finish_at', '>', Carbon::now()->format('Y-m-d H:i:s'));
+                });
+            })
+            ->firstOrFail();
+
         $template = $card->fields()->get()->groupBy('group');
 
         $ecard = [
