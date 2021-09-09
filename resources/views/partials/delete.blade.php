e8f439a80b91f672022c08da80753024b8b81506
Jose Nieto <inge1neuro@gmail.com>
2021-09-09 14:23:06 -0500

    Agregando dialogos de confirmación al borrar.

--- resources/views/partials/delete.blade.php
+++ resources/views/partials/delete.blade.php
@@ -1,12 +1,10 @@
 <div class="card mb-3">
     <div class="card-header">
         <div class="card-title">{{$label}}</div>
-
         <div class="card-tools">
-            <a class="btn btn-danger btn-sm" title="Borrar" href="{{$route}}"
-                onclick="event.preventDefault(); document.getElementById('{{$id_form}}').submit();">
+            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#{{$id_form}}Modal">
                 <i class="fa fa-trash" aria-hidden="true"></i>
-            </a>
+            </button>
         </div>
     </div>
 
@@ -14,4 +12,28 @@
         @csrf
         @method('DELETE')
     </form>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="{{$id_form}}Modal" tabindex="-1" role="dialog" aria-labelledby="{{$id_form}}ModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="{{$id_form}}ModalLabel">Borrar</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ¿Realmente desea borrar este registro?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary"
+                    onclick="document.getElementById('{{$id_form}}').submit();">
+                    Borrar
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
