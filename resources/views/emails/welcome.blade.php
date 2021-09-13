e3d17fc4acc8bf8c9489cdf0c094a0b6d436c1c3
Jose Nieto <inge1neuro@gmail.com>
2021-09-13 15:45:10 -0500

    Corrigiendo mensaje de bienvenida.

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1 style="text-align: center;">
-        {{$name}}, Bienvenido a iCard
+        {{$name}}, Le damos la bienvenida a iCard
     </h1>
     <p style="text-align: center;">
         Ya puedes ir a <a href="{{url('/login')}}">{{url('/login')}}</a> para ingresar tus tarjetas de presentación.
